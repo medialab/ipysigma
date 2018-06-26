ed77fcbb989dda6277ac9961571334c962d59cd7
Yomguithereal <guillaumeplique@gmail.com>
2018-06-26 18:00:35 +0200

    Sane default color for nodes

--- js/lib/sigma.js
+++ js/lib/sigma.js
@@ -76,7 +76,7 @@ function buildGraph(data) {
             attrs.size = _.get(attrs, 'viz.size', 2);
 
         if (!attrs.color)
-            attrs.color = 'color' in attrs.viz ? toRGBString(attrs.viz.color) : Math.random();
+            attrs.color = 'color' in attrs.viz ? toRGBString(attrs.viz.color) : '#BBB';
 
         if (!attrs.label)
             attrs.label = key;
